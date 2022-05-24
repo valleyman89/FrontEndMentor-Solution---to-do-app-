e32d0bdd5fe091dfc191930ef1dfae5b49f5106a
valleyman89 <dev@stevenrolph.com>
2022-05-24 13:34:19 +0100

    import check/delete components and call

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
+import Check from "./common/Check";
+import Delete from "./common/Delete";
 
 const Item = (props) => {
-  const { id, content } = props;
+  const { data, onDelete, onComplete } = props;
   return (
-    <div id={id}>
-      <li>{content}</li>
+    <div>
+      <li>
+        <Check id={data.id} complete={data.completed} onComplete={onComplete} />{" "}
+        {data.content}
+        <Delete id={data.id} onDelete={onDelete} />
+      </li>
     </div>
   );
 };
