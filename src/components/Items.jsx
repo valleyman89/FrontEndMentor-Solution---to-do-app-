ac8d2baff459eb48ae6553bf5f7a26668539290e
valleyman89 <dev@stevenrolph.com>
2022-05-24 13:34:36 +0100

    tidy up item call and set key

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
-import { getItems } from "../services/items";
 import Item from "./Item";
 
 const Items = (props) => {
-  const [items, setItems] = useState(getItems);
-
+  const { data, onDelete, onComplete } = props;
   return (
     <div>
-      {items.map((item) => {
-        return <Item id={item.id} content={item.content} />;
+      {data.map((item) => {
+        return (
+          <div key={item.id}>
+            <ul>
+              <Item data={item} onDelete={onDelete} onComplete={onComplete} />
+            </ul>
+          </div>
+        );
       })}
     </div>
   );
