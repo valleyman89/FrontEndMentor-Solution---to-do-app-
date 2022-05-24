52906e188b0eb952516021183c2cf82e3d4199a4
valleyman89 <dev@stevenrolph.com>
2022-05-24 13:33:27 +0100

    define Filter component

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const Filter = (props) => {
-  return <div>Filter</div>;
+  return <div>Filter: All | Active | Completed | Clear Completed</div>;
 };
 
 export default Filter;
