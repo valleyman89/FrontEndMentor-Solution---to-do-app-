ae06453dc1dcd5386a7e04bcdbc1f14fef8fc8af
valleyman89 <dev@stevenrolph.com>
2022-05-24 13:33:17 +0100

    define CreateItem component

---
+++ src/components/CreateItem.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+const CreateItem = (props) => {
+  const { data, onNewItem } = props;
+  const [item, setItem] = useState("");
+  const [id, setId] = useState(data.length + 1);
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    setId((prev) => prev + 1);
+
+    const newItem = {
+      id: id,
+      content: item,
+      completed: true,
+      orderId: 1,
+    };
+    onNewItem(newItem);
+    setItem("");
+  };
+
+  return (
+    <form onSubmit={handleOnSubmit}>
+      <input
+        type="text"
+        name="newItemInput"
+        value={item}
+        onChange={(e) => setItem(e.target.value)}
+        placeholder="Create a new todo..."
+        aria-label="newtask"
+      />
+    </form>
+  );
+};
+
+export default CreateItem;
