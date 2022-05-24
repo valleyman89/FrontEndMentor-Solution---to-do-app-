1093e58ebfda8aec96b52f66fde38f66d8a4ff5f
valleyman89 <dev@stevenrolph.com>
2022-05-24 13:34:55 +0100

    define handle functions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import "./App.css";
 import Items from "./components/Items";
-import CreateTask from "./components/CreateTask";
+import CreateItem from "./components/CreateItem";
 import Filter from "./components/Filter";
-import React from "react";
+import React, { useState } from "react";
+import { useLocalStorage } from "./utils/localStorage";
 
 function App() {
+  const [items, setItems] = useLocalStorage("items", "");
+
+  const handleNewItem = (item) => {
+    setItems([...items, item]);
+  };
+
+  const handleDeleteItem = (id) => {
+    const ogItems = items;
+    const newItems = ogItems.filter((c) => c.id !== id);
+    setItems(newItems);
+  };
+
+  const handleCompleteItem = (item, state) => {
+    console.log("handleCompleteItem" + item + " is " + state);
+  };
+
   return (
     <React.Fragment>
-      <CreateTask />
-      <Items />
+      <CreateItem data={items} onNewItem={handleNewItem} />
+      {items.length < 1 ? (
+        "nothing here"
+      ) : (
+        <Items
+          onDelete={handleDeleteItem}
+          onComplete={handleCompleteItem}
+          data={items}
+        />
+      )}
       <Filter />
     </React.Fragment>
   );
