2af1857630ec5b2719b9789b1f759f4ada3b5794
Andrew Griffiths <andrew@feedr.co>
2022-05-27 12:27:19 +0100

    helps to add stuff does it not

---
+++ src/ItemsContext.jsx
@@ -0,0 +1,21 @@
+import react from 'react'
+import { useLocalStorage } from "./utils/localStorage";
+
+const ItemsContext = react.createContext({})
+
+export const ItemsProvider = ({children}) => {
+  const [items, setItems] = useLocalStorage("items", []);
+
+  const filteredItems = items
+
+  return (
+    <ItemsContext.Provider value={{ items: filteredItems, setItems }}>
+      {children}
+    </ItemsContext.Provider>
+  )
+}
+
+export const useItemContext = () => {
+  const context = react.useContext(ItemsContext)
+  return context
+}
